3e38c16888636a423c52a33ea551faa17b68bb5a
chobuzz <chobuzz23@gmail.com>
2024-01-22 18:42:34 +0900

    Test: branch test 2

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <p>buzz branch test 1</p>
+        <p>buzz branch test 2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
