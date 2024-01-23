56c2c815c29ef3e3f1d27506bd661b48a696eb6e
chobuzz <chobuzz23@gmail.com>
2024-01-23 21:15:50 +0900

    feat: eslint 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+};
