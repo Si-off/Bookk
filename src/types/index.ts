0baaab45a4b53acd590c7c5d3ad5e2469be43c1d
당근 <noismik97@gmail.com>
2024-01-23 09:44:27 +0900

    feat: api 타입정의

---
+++ src/types/index.ts
@@ -0,0 +1,49 @@
+export type ImagesType = {
+  id: number;
+  updatedAt: string;
+  createdAt: string;
+  order: number;
+  type: number;
+  path: string;
+  fbPath: string[];
+};
+
+export type AuthorType = {
+  id: number;
+  nickname: string;
+  name: string;
+  email: string;
+  followerCount: number;
+  followeeCount: number;
+};
+
+export type BookInfoType = {
+  id: number;
+  updatedAt: string;
+  createdAt: string;
+  title: string;
+  content: string;
+  clicks: number;
+  likeCount: number;
+  reply2Count: number;
+  isSecret: boolean;
+  images: ImagesType[];
+  author: AuthorType;
+  api2cate: string;
+};
+
+export type GetBooklistRes = {
+  data: BookInfoType[];
+  cursor: {
+    after: number;
+  };
+  count: number;
+  next: string;
+  total: number;
+};
+
+export type PostBookParams = {
+  title: string;
+  content: string;
+  images?: string[];
+};
