0972f4b10f18a44c7e7a75ae91a95daa1209ca19
noismik <noismik97@gmail.com>
2024-01-24 15:15:39 +0900

    refactor: UserPage 스타일

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -14,10 +14,10 @@ const Book = ({ title, content, images, onClick }: Props) => {
 
   return (
     <Container onClick={onClick}>
-      <Wrap>
+      <Inner>
         <Title>{title}</Title>
         <Content>{content}</Content>
-      </Wrap>
+      </Inner>
       {images && <Image src={`${BASE_URL}${imageUrl}`} />}
     </Container>
   );
@@ -26,42 +26,55 @@ const Book = ({ title, content, images, onClick }: Props) => {
 export default Book;
 
 const Title = styled.p`
-  font-size: ${pixelToRem(24)};
+  font-size: ${pixelToRem(16)};
   font-weight: 700;
+
+  transition: font-size 1s;
+
   &::after {
     content: '';
     display: block;
     width: 30%;
     border-bottom: 2px solid rgba(0, 0, 0, 0);
     transition: border 1s ease;
-    margin-top: 10px;
+    margin: 10px 0px;
   }
+`;
+
+const Inner = styled.div`
+  display: flex;
+  flex-direction: column;
+  z-index: 2;
 `;
 
 const Image = styled.img`
   width: 150px;
+  border-radius: 4px;
   object-fit: cover;
+  opacity: 0.5;
+  filter: saturate(0.7);
+  transition: opacity 1s, filter 1s, transform 1s;
+  transform-origin: left center;
+  box-shadow: 5px 5px 30px rgba(0, 0, 0, 0.3);
 `;
 
 const Container = styled.div`
+  position: relative;
+  height: 500px;
+  user-select: none;
   display: inline-flex;
   flex-direction: column;
   align-items: center;
-  position: relative;
-  user-select: none;
-  z-index: 2;
+  justify-content: space-between;
+  z-index: 1;
   color: ${getStyledColor('gray', 900)};
-  width: 100%;
-  height: 300px;
-  padding: 10px 20px;
+
+  flex: 1;
+
+  padding: 30px 50px;
 
   transition: flex 1s ease, color 1s ease;
-
-  ${Image} {
-    opacity: 0.5;
-    filter: saturate(0.7);
-    transition: opacity 1s, filter 1s;
-  }
+  cursor: pointer;
 
   &:before {
     content: '';
@@ -69,22 +82,29 @@ const Container = styled.div`
     width: 80%;
     height: 95%;
     position: absolute;
-    background: #444;
+    background: ${getStyledColor('cool_gray', 1100)};
     top: 0px;
     left: -50px;
     opacity: 0;
     transition: opacity 1s, transform 1s;
+    border-radius: 4px;
   }
   &:hover {
     color: ${getStyledColor('gray', 400)};
-
-    ${Title}::after {
-      border-bottom: 2px solid red;
-    }
+    flex: 1.5;
 
     ${Image} {
       opacity: 1;
       filter: saturate(1);
+      transform: scale(1.1);
+    }
+
+    ${Title} {
+      font-size: ${pixelToRem(24)};
+    }
+
+    ${Title}::after {
+      border-bottom: 2px solid red;
     }
 
     &:before {
@@ -92,12 +112,6 @@ const Container = styled.div`
       opacity: 1;
     }
   }
-`;
-
-const Wrap = styled.div`
-  flex: 1;
-  z-index: 1;
-  height: 300px;
 `;
 
 const Content = styled.p`
--- src/pages/userpage/UserPage.tsx
+++ src/pages/userpage/UserPage.tsx
@@ -1,79 +1,35 @@
 import { useState } from 'react';
 import { styled } from 'styled-components';
-import { Link } from 'react-router-dom';
 import { getStyledColor } from 'utils';
-import { useUserStore } from 'store/useUserStore';
 import Book from '../../components/Book';
 import { useGetBooks } from 'queries';
 
 const UserPage = () => {
-  const user = useUserStore();
   const [currentPage, setCurrentPage] = useState(1);
-  const { data: books, status } = useGetBooks({ take: 4, page: currentPage });
+  const { data: books, status, isSuccess } = useGetBooks({ take: 4, page: currentPage });
 
   return (
-    <Wrap>
-      <Header>
-        {user.user ? (
-          <>
-            <Name>{user.user.nickname}</Name>
-            {/* 로그인 후 전역상태관리 User 체크 후에 수정 예정 */}
-          </>
-        ) : (
-          <>
-            <StyledLink to='/login'>로그인</StyledLink>
-            <StyledLink to='/signup'>회원가입</StyledLink>
-          </>
-        )}
-      </Header>
-      <Main>
-        <BookLayout>
-          {status === 'success' &&
-            books.data.map((book) => {
-              return <Book key={book.id} {...book} />;
-            })}
-        </BookLayout>
-      </Main>
-    </Wrap>
+    <Layout>
+      <BookWrapper $isSuccess={isSuccess}>
+        {status === 'success' &&
+          books.data.map((book) => {
+            return <Book key={book.id} {...book} />;
+          })}
+      </BookWrapper>
+    </Layout>
   );
 };
 
 export default UserPage;
 
-const Wrap = styled.div`
+const Layout = styled.div`
   height: 100vh;
-  overflow-y: hidden;
-  background-color: ${getStyledColor('gray', 1100)};
+  background-color: ${getStyledColor('cool_gray', 1200)};
 `;
 
-const Header = styled.header`
-  position: absolute;
-  width: 100%;
-  padding: 30px;
-  background-color: ${getStyledColor('gray', 1200)};
-`;
-const Main = styled.main`
+const BookWrapper = styled.div<{ $isSuccess?: boolean }>`
   display: flex;
-  padding: 30px 0px;
-  overflow-x: hidden;
-  align-items: flex-end;
-  height: 100%;
-`;
-
-const StyledLink = styled(Link)`
-  color: #fff;
-  text-decoration: none;
-  transition: color 0.2s ease;
-
-  &:hover {
-    color: ${getStyledColor('red', 900)};
-  }
-`;
-
-const Name = styled.div`
-  color: #fff;
-`;
-
-const BookLayout = styled.div`
-  display: flex;
+  width: 70%;
+  transition: opacity 1s ease;
+  opacity: ${({ $isSuccess }) => ($isSuccess ? 1 : 0)};
 `;
