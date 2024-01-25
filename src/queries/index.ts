06f4fc2e5beb765d76d41b164939aaf592fd3388
noismik <noismik97@gmail.com>
2024-01-25 16:33:21 +0900

    feat: useLogin

--- src/queries/index.ts
+++ src/queries/index.ts
@@ -1,11 +1,17 @@
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { getBooks, postBooks, patchBook, deleteBook, getBook } from "api";
-import { BooklistParams, BooklistRes } from "types";
-import queryKeys from "./queryKeys";
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { getBooks, postBooks, patchBook, deleteBook, getBook } from 'api';
+import { BooklistParams, BooklistRes, UserState } from 'types';
+import queryKeys from './queryKeys';
+import { login, reLogin } from 'api/auth';
+import CustomAxiosInstance from 'api/axios';
+import { StorageKeys } from 'constant';
+import secureLocalStorage from 'react-secure-storage';
+import { useNavigate } from 'react-router-dom';
+import { useUserStore } from 'store/useUserStore';
 
 export const useGetBooks = (queries: BooklistParams) => {
   const queryClient = useQueryClient();
-  const key = [queryKeys.USER, "books"];
+  const key = [queryKeys.USER, 'books'];
   if (queries) key.push(queries.page.toString());
 
   return useQuery({
@@ -16,7 +22,7 @@ export const useGetBooks = (queries: BooklistParams) => {
       if (res.total < queries.take * queries.page) return;
 
       await queryClient.prefetchQuery({
-        queryKey: [queryKeys.USER, "books", (queries.page + 1).toString()],
+        queryKey: [queryKeys.USER, 'books', (queries.page + 1).toString()],
         queryFn: () => getBooks({ ...queries, page: queries.page + 1 }),
         staleTime: 1000 * 60 * 3,
         cacheTime: 1000 * 60 * 5,
@@ -29,7 +35,7 @@ export const useGetBooks = (queries: BooklistParams) => {
 };
 export const useGetBooksAdmin = (queries: BooklistParams) => {
   const queryClient = useQueryClient();
-  const key = [queryKeys.ADMIN, "books"];
+  const key = [queryKeys.USER, 'books'];
   if (queries) key.push(queries.page.toString());
 
   return useQuery({
@@ -40,7 +46,7 @@ export const useGetBooksAdmin = (queries: BooklistParams) => {
       if (res.total < queries.take * queries.page) return;
 
       await queryClient.prefetchQuery({
-        queryKey: [queryKeys.ADMIN, "books", (queries.page + 1).toString()],
+        queryKey: [queryKeys.USER, 'books', (queries.page + 1).toString()],
         queryFn: () => getBooks({ ...queries, page: queries.page + 1 }),
         staleTime: 1000 * 60 * 3,
         cacheTime: 1000 * 60 * 5,
@@ -51,7 +57,7 @@ export const useGetBooksAdmin = (queries: BooklistParams) => {
 
 export const useGetBook = (id: number) => {
   const queryClient = useQueryClient();
-  const key = [queryKeys.ADMIN, "books", id.toString()];
+  const key = [queryKeys.ADMIN, 'books', id.toString()];
   return useQuery({
     queryKey: key,
     queryFn: () => getBook(id),
@@ -63,11 +69,11 @@ export const usePostBook = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationKey: [queryKeys.ADMIN, "books"],
+    mutationKey: [queryKeys.ADMIN, 'books'],
     mutationFn: postBooks,
     onSuccess: () => {
-      queryClient.invalidateQueries([queryKeys.ADMIN, "books"]);
-      queryClient.invalidateQueries([queryKeys.USER, "books"]);
+      queryClient.invalidateQueries([queryKeys.ADMIN, 'books']);
+      queryClient.invalidateQueries([queryKeys.USER, 'books']);
     },
   });
 };
@@ -76,11 +82,11 @@ export const usePatchBook = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationKey: [queryKeys.ADMIN, "books"],
+    mutationKey: [queryKeys.ADMIN, 'books'],
     mutationFn: patchBook,
     onSuccess: () => {
-      queryClient.invalidateQueries([queryKeys.ADMIN, "books"]);
-      queryClient.invalidateQueries([queryKeys.USER, "books"]);
+      queryClient.invalidateQueries([queryKeys.ADMIN, 'books']);
+      queryClient.invalidateQueries([queryKeys.USER, 'books']);
     },
   });
 };
@@ -89,11 +95,43 @@ export const useDeleteBook = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationKey: [queryKeys.ADMIN, "books"],
+    mutationKey: [queryKeys.ADMIN, 'books'],
     mutationFn: deleteBook,
     onSuccess: () => {
-      queryClient.invalidateQueries([queryKeys.ADMIN, "books"]);
-      queryClient.invalidateQueries([queryKeys.USER, "books"]);
+      queryClient.invalidateQueries([queryKeys.ADMIN, 'books']);
+      queryClient.invalidateQueries([queryKeys.USER, 'books']);
     },
   });
+};
+
+export const useLogin = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  // TODO 추후 제거
+  const setUser = useUserStore((state: UserState) => state.setUser);
+  const setAccessToken = useUserStore((state: UserState) => state.setAccessToken);
+
+  return useMutation({
+    mutationKey: [queryKeys.USER],
+    mutationFn: login,
+    onSuccess: (data) => {
+      if (!data) return;
+
+      // TODO 추후 제거
+      setUser(data.userInfo);
+      setAccessToken(data.accessToken);
+
+      console.log(data);
+
+      queryClient.setQueryData([queryKeys.USER], data.userInfo);
+      CustomAxiosInstance.setAccessToken(data.accessToken);
+      secureLocalStorage.setItem(StorageKeys.REFRESH_TOKEN, data.refreshToken);
+      navigate('/user');
+    },
+  });
+};
+
+export const useReLogin = (token: string) => {
+  return useQuery({ queryKey: [queryKeys.USER], queryFn: reLogin, enabled: !!token });
 };
