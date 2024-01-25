2dfed5e0f4e73fbddbbc872c8563ca30892c1f78
jangco97 <qkrnslehd@naver.com>
2024-01-25 18:53:50 +0900

    feat: 커스텀 모달 adminpage에 적용(추후 userpage에도 적용예정)

---
+++ src/components/modal/CustomModal.tsx
@@ -0,0 +1,52 @@
+import { useRef, useState } from "react";
+import * as S from "styles/ModalStyled";
+import { BookRes } from "types";
+import { useGetBook } from "queries";
+import useOnclickOutside from "pages/hooks/useOnclickOutside";
+export const CustomModal = ({
+  bookId,
+  setModalOpen,
+  showScroll,
+}: {
+  bookId: number | null;
+  setModalOpen: (open: boolean) => void;
+  showScroll: () => void;
+}) => {
+  const ref = useRef(null);
+  useOnclickOutside(ref, () => {
+    setModalOpen(false);
+    showScroll();
+  });
+  const { data: book, status } = useGetBook(bookId || 0);
+
+  if (status === "loading") return <div>loading...</div>;
+  if (status === "error") return <div>error...</div>;
+
+  return (
+    <S.Presentation role="presentation">
+      <S.WrapperModal>
+        <S.Modal ref={ref}>
+          <S.ModalClose
+            onClick={() => {
+              setModalOpen(false);
+              showScroll();
+            }}
+          >
+            X
+          </S.ModalClose>
+          <S.ModalPosterImg src={book?.images[0].fbPath[0]} alt="modal-img" />
+          <S.ModalContent>
+            <S.ModalDetails>등록날짜:{book?.createdAt}</S.ModalDetails>
+            <S.ModalTitle>{book?.title}</S.ModalTitle>
+            <S.ModalOverview>클릭수: {book?.clicks}</S.ModalOverview>
+            <S.ModalOverview>좋아요: {book?.likeCount}</S.ModalOverview>
+            <S.ModalOverview>작성자: {book?.author.name}</S.ModalOverview>
+            <S.ModalOverview>{book?.content}</S.ModalOverview>
+          </S.ModalContent>
+        </S.Modal>
+      </S.WrapperModal>
+    </S.Presentation>
+  );
+};
+
+export default CustomModal;
---
+++ src/styles/ModalStyled.ts
@@ -0,0 +1,101 @@
+import styled, { keyframes } from "styled-components";
+
+export const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: scale(0.5);
+  }
+  to {
+    opacity: 1;
+    transform: scale(1);
+  }
+`;
+
+export const WrapperModal = styled.div`
+  position: fixed;
+  inset: 0px;
+  background-color: rgb(0 0 0 / 71%);
+  display: flex;
+  justify-content: center;
+  overflow: hidden;
+  height: 100%;
+
+  @media screen and (max-height: 768px) {
+    align-items: unset;
+    padding-top: 2rem;
+  }
+
+  @media screen and (max-width: 768px) {
+    padding: 0;
+  }
+`;
+
+export const Presentation = styled.div`
+  z-index: 1200;
+  position: absolute;
+`;
+
+export const ModalClose = styled.div`
+  position: absolute;
+  right: 20px;
+  top: 20px;
+  cursor: pointer;
+  z-index: 1000;
+  color: white;
+`;
+
+export const Modal = styled.div`
+  position: relative;
+  max-width: 800px;
+  box-shadow: 0px 3px 5px -1px rgba(0, 0, 0, 0.2),
+    0px 5px 8px 0px rgba(0, 0, 0, 0.14), 0px 1px 14px 0px rgba(0, 0, 0, 0.12);
+  background: #111;
+  overflow: hidden;
+  border-radius: 8px;
+  transition: all 400ms ease-in-out 2s;
+  animation: ${fadeIn} 400ms;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    display: none;
+    visibility: hidden;
+  }
+
+  @media screen and (max-width: 768px) {
+    overflow-y: scroll !important;
+  }
+`;
+
+export const ModalPosterImg = styled.img`
+  width: 100%;
+  height: auto;
+`;
+
+export const ModalContent = styled.div`
+  padding: 40px;
+  color: white;
+`;
+
+export const ModalTitle = styled.h1`
+  padding: 0;
+  font-size: 40px;
+  margin: 16px 0;
+`;
+
+export const ModalDetails = styled.div`
+  font-weight: 600;
+  font-size: 18px;
+
+  @media screen and (max-width: 768px) {
+    font-size: 16px;
+  }
+`;
+
+export const ModalOverview = styled.p`
+  font-size: 20px;
+  line-height: 1.5;
+
+  @media screen and (max-width: 768px) {
+    font-size: 16px;
+  }
+`;
