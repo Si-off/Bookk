3d1605604a1671c8010d71b7013372622deae84f
jangco97 <qkrnslehd@naver.com>
2024-01-26 14:50:11 +0900

    feat: 유저페이지 모달에 댓글 불러오는 토글 생성

---
+++ src/components/CommentToggle.tsx
@@ -0,0 +1,57 @@
+import { useGetComments } from "queries";
+import React from "react";
+import styled from "styled-components";
+import { StyledLoader } from "styles/LoginStyled";
+import * as S from "styles/CommentStyled";
+import { useUserStore } from "store/useUserStore";
+interface CommentToggleProps {
+  toggleModal: () => void;
+  isOpen: boolean;
+  bookId: number | undefined;
+}
+const CommentToggle = ({ toggleModal, isOpen, bookId }: CommentToggleProps) => {
+  if (bookId === undefined) return null;
+  const { data: comments, status } = useGetComments(bookId);
+  const user = useUserStore((state) => state.user);
+  console.log(user);
+  if (status === "loading") return <StyledLoader />;
+  if (status === "error") return <div>error</div>;
+  return (
+    <Toggle isOpen={isOpen}>
+      <S.CommentContainer>
+        {comments?.data?.map((reply: any, index: any) => {
+          return (
+            <S.CommentItemContainer key={reply.id} $index={index}>
+              <div>
+                <span>작성자:{reply.author.nickname}</span>
+                <span>
+                  <div>{reply.reply2}</div>
+                </span>
+              </div>
+
+              <S.CommentItemRight>
+                <span>{reply.createdAt}</span>
+                {/* {user?.nickname === reply?.author?.nickname && (
+                <S.CommentButtonContainer>
+                  <S.CommentButton>수정</S.CommentButton>
+                  <S.CommentButton>삭제</S.CommentButton>
+                </S.CommentButtonContainer>
+              )} */}
+              </S.CommentItemRight>
+            </S.CommentItemContainer>
+          );
+        })}
+      </S.CommentContainer>
+      <button onClick={toggleModal}>toggle</button>
+    </Toggle>
+  );
+};
+
+export default CommentToggle;
+
+const Toggle = styled.div<{ isOpen: boolean }>`
+  display: ${({ isOpen }) => (isOpen ? "block" : "none")};
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.4);
+`;
