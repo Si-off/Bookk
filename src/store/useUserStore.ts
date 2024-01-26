95a029730654b3e4887b0b5e624e08376cdae3b4
jangco97 <qkrnslehd@naver.com>
2024-01-26 14:51:00 +0900

    useuserstore에 user정보 넣기 추기

--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -1,13 +1,19 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 interface UserStore {
+  user: null;
   isLogin: boolean;
   setIsLogin: (state: boolean) => void;
+  setUser: (user: any) => void;
 }
 
 export const useUserStore = create<UserStore>((set) => ({
+  user: null,
   isLogin: false,
   setIsLogin: (state) => {
     set({ isLogin: state });
+  },
+  setUser: (user) => {
+    set({ user });
   },
 }));
