74b1b0e86e498c99b484b1b63cecd9969d0a6cb8
noismik <noismik97@gmail.com>
2024-01-26 15:27:25 +0900

    test: user data

--- src/components/CommentToggle.tsx
+++ src/components/CommentToggle.tsx
@@ -1,9 +1,11 @@
-import { useGetComments } from "queries";
-import React from "react";
-import styled from "styled-components";
-import { StyledLoader } from "styles/LoginStyled";
-import * as S from "styles/CommentStyled";
-import { useUserStore } from "store/useUserStore";
+import { useGetComments } from 'queries';
+import React from 'react';
+import styled from 'styled-components';
+import { StyledLoader } from 'styles/LoginStyled';
+import * as S from 'styles/CommentStyled';
+import { useUserStore } from 'store/useUserStore';
+import { useQueryClient } from '@tanstack/react-query';
+import { QueryKeys } from 'constant';
 interface CommentToggleProps {
   toggleModal: () => void;
   isOpen: boolean;
@@ -12,10 +14,13 @@ interface CommentToggleProps {
 const CommentToggle = ({ toggleModal, isOpen, bookId }: CommentToggleProps) => {
   if (bookId === undefined) return null;
   const { data: comments, status } = useGetComments(bookId);
-  const user = useUserStore((state) => state.user);
+  // const user = useUserStore((state) => state.user);
+  const user = useQueryClient().getQueryData([QueryKeys.LOGIN]);
   console.log(user);
-  if (status === "loading") return <StyledLoader />;
-  if (status === "error") return <div>error</div>;
+
+  // console.log(user);
+  if (status === 'loading') return <StyledLoader />;
+  if (status === 'error') return <div>error</div>;
   return (
     <Toggle isOpen={isOpen}>
       <S.CommentContainer>
@@ -50,7 +55,7 @@ const CommentToggle = ({ toggleModal, isOpen, bookId }: CommentToggleProps) => {
 export default CommentToggle;
 
 const Toggle = styled.div<{ isOpen: boolean }>`
-  display: ${({ isOpen }) => (isOpen ? "block" : "none")};
+  display: ${({ isOpen }) => (isOpen ? 'block' : 'none')};
   width: 100%;
   height: 100%;
   background-color: rgba(0, 0, 0, 0.4);
