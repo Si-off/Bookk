03df142d36f15b745f8f0905ee26cb41a4abbf62
noismik <noismik97@gmail.com>
2024-01-29 14:44:54 +0900

    refactor: 중첩라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { useEffect } from 'react';
 import Navigation from './components/layout/Navigation';
-import AdminManage from './pages/adminpage/AdminManage';
-import AdminCreateItem from './pages/adminpage/AdminCreateItem';
-import AdminEditItem from './pages/adminpage/AdminEditItem';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { LoginPage, SignupPage } from 'pages';
 import CustomAxiosInstance from 'api/axios';
@@ -13,6 +10,7 @@ import PrivateRoutes from 'pages/PrivateRoutes';
 import { useQueryClient } from '@tanstack/react-query';
 import { getUser } from 'api/auth';
 import UserPage from 'pages/UserPage';
+import { AdminManage, AdminCreateItem, AdminEditItem } from 'pages/adminpage';
 
 function App() {
   const { isLogin, setIsLogin, setIsInit } = useUserStore();
@@ -35,12 +33,13 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Navigation />
       <Routes>
-        <Route path='/' element={<UserPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/signup' element={<SignupPage />} />
-        <Route path='*' element={<Navigate to='/' />} />
+        <Route element={<Navigation />}>
+          <Route path='/' element={<UserPage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/signup' element={<SignupPage />} />
+          <Route path='*' element={<Navigate to='/' />} />
+        </Route>
 
         <Route element={<PrivateRoutes />}>
           <Route path='/admin' element={<AdminManage />} />
