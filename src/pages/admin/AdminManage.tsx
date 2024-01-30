cd0f4cc93c9afe1b4bc6c8490333975b4b1a7e58
noismik <noismik97@gmail.com>
2024-01-30 14:52:30 +0900

    refactor: adminmanage 스타일

--- src/pages/admin/AdminManage.tsx
+++ src/pages/admin/AdminManage.tsx
@@ -8,9 +8,10 @@ import { useDeleteBook, useGetBooksAdmin } from 'queries';
 import { getStyledColor } from 'utils';
 import { useSelectedBook } from 'store/useSelectedBooks';
 import { getDateStr } from 'utils';
-import { BookInfoType, BookTakelistRes } from 'types';
+import { BookInfoType } from 'types';
 import { useQueryClient } from '@tanstack/react-query';
 import { getNextBooks } from 'api';
+import { CustomModal } from 'components/modal/CustomModal';
 import { QueryKeys } from 'constant';
 import Loader from 'components/shared/Loader';
 
@@ -49,7 +50,7 @@ const AdminManage = () => {
   }, [currentPage]);
 
   const { setSelectedBook } = useSelectedBook();
-  const { mutate: remove, isLoading } = useDeleteBook();
+  const { mutate: remove } = useDeleteBook();
 
   const navigate = useNavigate();
 
@@ -69,15 +70,9 @@ const AdminManage = () => {
     setSelectedBookId(id); // 선택된 책의 ID를 상태에 저장
   };
 
-  const goToCreate = () => {
-    navigate('/admin/create');
-  };
-
-  const handleEdit = (id: any) => {
+  const handleEdit = (id: number) => {
     if (!books) return;
-    const selectedBook: BookInfoType | undefined = books.data.find(
-      (book) => book.id === parseInt(id)
-    );
+    const selectedBook: BookInfoType | undefined = books.data.find((book) => book.id === id);
     if (!selectedBook) return;
     const { images, title, content } = selectedBook;
 
@@ -85,7 +80,7 @@ const AdminManage = () => {
     navigate(`/admin/edit/${id}`);
   };
 
-  const handleRemove = (id: any) => {
+  const handleRemove = (id: number) => {
     remove(id);
   };
   const handlePageClick = (pageNum: number) => {
@@ -108,77 +103,76 @@ const AdminManage = () => {
     );
   }
 
-  // type bookType = Pick<BookTakelistRes, 'data'>;
-  // const renderTableRow = (book: bookType, index: number) => {
-  //   const { id, title, createdAt, updatedAt, likeCount, clicks, reply2Count, author } = book;
-
-  //   // 데이터가 존재하는 경우에만 출력
-  //   return (
-  //     <S.Trow key={id}>
-  //       <S.Tcell>{(currentPage - 1) * 10 + index + 1}</S.Tcell>
-  //       <S.Tcell>{id}</S.Tcell>
-  //       <S.Tcell>
-  //         <button onClick={() => handleClick(id)}>{title}</button>
-  //       </S.Tcell>
-  //       <S.Tcell>{author.name}</S.Tcell>
-  //       <S.Tcell>{clicks}</S.Tcell>
-  //       <S.Tcell>{likeCount}</S.Tcell>
-  //       <S.Tcell>{reply2Count}</S.Tcell>
-  //       <S.Tcell>{getDateStr(createdAt)}</S.Tcell>
-  //       <S.Tcell>
-  //         <EditIcon onClick={() => handleEdit(id)} />
-  //         <TrashIcon onClick={() => handleRemove(id)} />
-  //       </S.Tcell>
-  //     </S.Trow>
-  //   );
-  // };
-
-  // // 테이블 바디 렌더링
-  // const renderTableBody = () => {
-  //   let renderedRows = [];
-
-  //   // 데이터가 존재하는 행을 렌더링
-  //   if (status === 'success') {
-  //     renderedRows = books.data.map((book, index) => renderTableRow(book, index));
-  //   }
-
-  //   // 빈 행을 추가하여 테이블의 크기를 고정
-  //   const emptyRowCount = 10 - renderedRows.length;
-  //   for (let i = 0; i < emptyRowCount; i++) {
-  //     renderedRows.push(
-  //       <S.Trow key={`empty-${i}`}>
-  //         {[...Array(9)].map((_, index) => (
-  //           <S.Tcell key={`empty-cell-${index}`} />
-  //         ))}
-  //       </S.Trow>
-  //     );
-  //   }
-
-  //   return renderedRows;
-  // };
-
   return (
     <S.Layout>
       <S.Container hidden={booksLoading}>
-        <ButtonContainer>
-          <CreateButton onClick={() => goToCreate()}>책 등록하기</CreateButton>
-        </ButtonContainer>
-        {/* <S.Tbody>{renderTableBody()}</S.Tbody> */}
+        <S.Table>
+          <S.Theader>
+            <S.Trow>
+              <S.Tcolumn>No.</S.Tcolumn>
+              <S.Tcolumn>ID</S.Tcolumn>
+              <S.Tcolumn>제목</S.Tcolumn>
+              <S.Tcolumn>생성자</S.Tcolumn>
+              <S.Tcolumn>조회수</S.Tcolumn>
+              <S.Tcolumn>좋아요</S.Tcolumn>
+              <S.Tcolumn>댓글</S.Tcolumn>
+              <S.Tcolumn>생성일</S.Tcolumn>
+              <S.Tcolumn />
+            </S.Trow>
+          </S.Theader>
+          <S.Tbody>
+            {status === 'success' &&
+              books.data.map((book, index) => {
+                const { id, title, createdAt, likeCount, clicks, reply2Count, author } = book;
+                return (
+                  <Fragment key={id}>
+                    <S.Trow>
+                      <S.Tcell>{(currentPage - 1) * 10 + index + 1}</S.Tcell>
+                      <S.Tcell>{id}</S.Tcell>
+                      <S.Tcell>
+                        <button onClick={() => handleClick(id)}>{title}</button>
+                      </S.Tcell>
+                      <S.Tcell>{author.name}</S.Tcell>
+                      <S.Tcell>{clicks}</S.Tcell>
+                      <S.Tcell>{likeCount}</S.Tcell>
+                      <S.Tcell>{reply2Count}</S.Tcell>
+                      <S.Tcell>{getDateStr(createdAt)}</S.Tcell>
+                      <S.Tcell>
+                        <EditIcon onClick={() => handleEdit(id)} />
+                        <TrashIcon onClick={() => handleRemove(id)} />
+                      </S.Tcell>
+                    </S.Trow>
+                    {modalOpen && (
+                      <CustomModal
+                        bookId={selectedBookId}
+                        book={selectedBook}
+                        setModalOpen={setModalOpen}
+                        showScroll={showScroll}
+                      ></CustomModal>
+                    )}
+                  </Fragment>
+                );
+              })}
+          </S.Tbody>
+        </S.Table>
         <div
           style={{
             display: 'flex',
             justifyContent: 'center',
             marginTop: '16px',
-          }}>
+          }}
+        >
           <Pagination>
             <PButton>
               <FaAngleLeft onClick={() => handlePageClick(currentPage - 1)} />
             </PButton>
-            {Array.from({ length: Math.ceil(books?.total / 10) }, (_, index) => (
-              <PNumber key={index} onClick={() => handlePageClick(index + 1)}>
-                {index + 1}
-              </PNumber>
-            ))}
+            <div>
+              {Array.from({ length: Math.ceil(books?.total / 10) }, (_, index) => (
+                <PNumber key={index} onClick={() => handlePageClick(index + 1)}>
+                  {index + 1}
+                </PNumber>
+              ))}
+            </div>
             <PButton>
               <FaAngleRight onClick={() => handlePageClick(currentPage + 1)} />
             </PButton>
@@ -221,22 +215,23 @@ const Pagination = styled.nav`
   display: inline-flex;
   justify-content: center;
   align-items: center;
-  gap: 8px;
+  gap: 30px;
 `;
 
 const PNumber = styled.button`
-  padding: 8px;
+  padding: 8px 12px;
   background-color: #fff;
-  border: 1px solid ${getStyledColor('blue', 400)};
   border-radius: 4px;
   transition: background-color 0.2s ease;
 
   &:hover {
-    background-color: ${getStyledColor('blue', 400)};
+    color: ${getStyledColor('white', 'high')};
+    background-color: ${getStyledColor('teal', 900)};
   }
 
   &:active {
-    background-color: ${getStyledColor('blue', 500)};
+    color: ${getStyledColor('white', 'high')};
+    background-color: ${getStyledColor('teal', 1000)};
   }
 `;
 
@@ -244,26 +239,4 @@ const PButton = styled.button`
   display: inline-flex;
   align-items: center;
   background-color: #fff;
-`;
-
-const Layout = styled.div`
-  min-width: 1200px;
-  padding: 45px;
-  overflow-x: scroll;
-  height: 100vh;
-  background-color: white;
-`;
-
-const CreateButton = styled.div`
-  background-color: #008cba;
-  padding: 10px 20px;
-  color: white;
-  cursor: pointer;
-  margin-bottom: 20px;
-  justify-content: start;
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  justify-content: end;
 `;
