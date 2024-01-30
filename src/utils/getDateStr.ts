30d6966ebac497ad1054c198c815e72ffe8d5aa8
noismik <noismik97@gmail.com>
2024-01-30 14:22:32 +0900

    chore: eslint & prettier

--- src/utils/getDateStr.ts
+++ src/utils/getDateStr.ts
@@ -1,12 +1,11 @@
 const makeDateStr = (date: string) => {
   const temp = new Date(date);
 
-  let year, month, day, hour, minute;
-  year = temp.getFullYear();
-  month = temp.getMonth() + 1;
-  day = temp.getDate();
-  hour = temp.getHours();
-  minute = temp.getMinutes();
+  const year = temp.getFullYear();
+  const month = temp.getMonth() + 1;
+  const day = temp.getDate();
+  const hour = temp.getHours();
+  const minute = temp.getMinutes();
 
   return `${year}년 ${month}월 ${day}일 ${hour}시 ${minute}분`;
 };
