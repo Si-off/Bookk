cdbb4eac15c32cec8073949820416ab351187636
chobuzz <chobuzz23@gmail.com>
2024-01-31 19:14:17 +0900

    style: MyPage

--- src/pages/user/MyPage.tsx
+++ src/pages/user/MyPage.tsx
@@ -71,12 +71,11 @@ const Wrapper = styled.div`
   color: rgba(31, 31, 31, 0.7);
   font-weight: 900;
   padding-top: 80px;
-  grid-auto-rows: minmax(100px, auto);
-  grid-template-rows: repeat(2, 1fr);
 
   .one {
     grid-column: 2 / -2;
     grid-row: 1;
+    height: 400px;
     border-radius: 8.889px;
     border: 1.778px solid #ebebee;
     background: #fff;
@@ -90,18 +89,16 @@ const Wrapper = styled.div`
 
   .two {
     grid-column: 2 / -2;
-    grid-row: 2;
     margin-top: 20px;
     color: white;
   }
 `;
 
 const Layout = styled.div`
-  height: 100vh;
-  background-color: ${getStyledColor('cool_gray', 1200)};
   display: flex;
   justify-content: center;
   align-items: center;
+  margin: 40px 0;
 `;
 
 const BookWrapper = styled.div<{ $isSuccess?: boolean }>`
