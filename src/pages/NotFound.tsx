15392e2fba10538b7f125e23868f4a7999163529
jangco97 <qkrnslehd@naver.com>
2024-02-01 11:55:52 +0900

    fix: 검색 결과 안나올 경우 해당 컴포넌트 보여주기

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,9 @@
-const NotFound = () => {
-  return <div>Not FOund</div>;
+const NotFound = (search: string) => {
+  return (
+    <div>
+      <h1>{search}에 대한 검색 결과가 없습니다.</h1>
+    </div>
+  );
 };
 
 export default NotFound;
