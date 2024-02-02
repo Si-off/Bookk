494a0ed244da5aca33b77764765a94d09e65e448
noismik <noismik97@gmail.com>
2024-02-03 08:03:48 +0900

    refactor: crypto 타입

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -2,16 +2,14 @@ import CryptoJS from 'crypto-js';
 const KEY = process.env.REACT_APP_SECURE_LOCAL_STORAGE_HASH_KEY;
 
 export const encrypt = (value: string) => {
+  if (!KEY) return '';
+
   return CryptoJS.AES.encrypt(value, KEY).toString();
 };
 
 export const decrypt = (value: string) => {
-  try {
-    const bytes = CryptoJS.AES.decrypt(value, KEY);
+  if (!KEY) return '';
+  const bytes = CryptoJS.AES.decrypt(value, KEY);
 
-    return bytes.toString(CryptoJS.enc.Utf8);
-  } catch (error) {
-    console.error(error);
-    return '';
-  }
+  return bytes.toString(CryptoJS.enc.Utf8);
 };
