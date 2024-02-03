c54c9f414f03b611be6b531ad8ccf9780e795cd1
jangco97 <qkrnslehd@naver.com>
2024-02-03 15:10:44 +0900

    fix: 아이콘 클릭시 댓글 목록 보기 기능 추가

--- src/pages/admin/AdminManageReviews.tsx
+++ src/pages/admin/AdminManageReviews.tsx
@@ -3,22 +3,28 @@ import { FaAngleLeft, FaAngleRight } from 'react-icons/fa6';
 import useAdminManage from 'hooks/useAdminManage';
 import * as S from 'styles/AdminStyledTemp';
 import { useGetReplies } from 'queries';
+import styled from 'styled-components';
 import { Loader } from 'components/shared';
 import { Replies } from 'types';
+import { IoIosArrowDropupCircle } from 'react-icons/io';
+import { IoIosArrowDropdownCircle } from 'react-icons/io';
+import { getDateStr } from 'utils';
+
 const AdminManageReviews = () => {
   const { currentPage, setCurrentPage, handleNextPage } = useAdminManage();
   const { data: reviews, status, isLoading } = useGetReplies();
-  const [selectedReviewId, setSelectedReviewId] = useState<number | null>(null);
+  const [selectedReviewId, setSelectedReviewId] = useState<number | null>(null); // 단일 ID로 변경
   useEffect(() => {
     console.log('reviews', reviews);
   }, [reviews]);
   const toggleDetail = (id: number) => {
     if (selectedReviewId === id) {
-      setSelectedReviewId(null); // 이미 선택된 아이템을 다시 클릭하면 닫힘
+      setSelectedReviewId(null); // 이미 열린 항목을 다시 클릭하면 닫음
     } else {
-      setSelectedReviewId(id); // 다른 아이템을 클릭하면 해당 아이템의 상세 정보 표시
+      setSelectedReviewId(id); // 다른 항목을 클릭하면 해당 항목의 상세 정보 표시
     }
   };
+
   if (!reviews || isLoading) {
     return <Loader />;
   }
@@ -35,6 +41,7 @@ const AdminManageReviews = () => {
               <S.Tcolumn>ID</S.Tcolumn>
               <S.Tcolumn>책이름</S.Tcolumn>
               <S.Tcolumn>댓글수</S.Tcolumn>
+              <S.Tcolumn>상세보기</S.Tcolumn>
             </S.Trow>
           </S.Theader>
           <S.Tbody>
@@ -42,20 +49,35 @@ const AdminManageReviews = () => {
               reviews?.map((review: Replies, index: number) => {
                 return (
                   <Fragment key={review.id}>
-                    <S.Trow onClick={() => toggleDetail(review.id)}>
+                    <S.Trow>
                       <S.Tcell width={30}>{(currentPage - 1) * 10 + index + 1}</S.Tcell>
                       <S.Tcell width={50}>{review.id}</S.Tcell>
                       <S.Tcell width={100}>{review.title}</S.Tcell>
                       <S.Tcell>{review.reply2Count}</S.Tcell>
+                      <S.Tcell>
+                        {selectedReviewId === review.id ? (
+                          <IoIosArrowDropupCircle
+                            size={24}
+                            onClick={() => toggleDetail(review.id)}
+                          />
+                        ) : (
+                          <IoIosArrowDropdownCircle
+                            size={24}
+                            onClick={() => toggleDetail(review.id)}
+                          />
+                        )}
+                      </S.Tcell>
                     </S.Trow>
                     {selectedReviewId === review.id && (
                       <S.Trow>
-                        <S.Tcell colSpan={4}>
-                          {/* 상세 댓글 정보 렌더링, 예: review.reply2s.map(...) */}
+                        <S.Tcell colSpan={5}>
+                          {/* 상세 댓글 정보 렌더링 */}
                           {review.reply2s.map((reply2) => (
-                            <div key={reply2.id}>
-                              <p>{reply2.reply2}</p>
-                            </div>
+                            <Wrapper key={reply2.id}>
+                              <p>ID:{reply2.id}</p>
+                              <p>내용:{reply2.reply2}</p>
+                              <p>작성일:{getDateStr(reply2.createdAt)}</p>
+                            </Wrapper>
                           ))}
                         </S.Tcell>
                       </S.Trow>
@@ -94,3 +116,12 @@ const AdminManageReviews = () => {
 };
 
 export default AdminManageReviews;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  border: 1px solid #e9e9e9;
+  padding: 10px;
+  margin: 10px 0;
+  border-radius: 10px;
+`;
