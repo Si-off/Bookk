e9f1643e1cd8d255840484f06175190438754326
jangco97 <qkrnslehd@naver.com>
2024-02-04 17:30:22 +0900

    chore: outsideclick 코드 복귀

--- src/components/modal/CustomModal.tsx
+++ src/components/modal/CustomModal.tsx
@@ -58,14 +58,12 @@ export const CustomModal = ({
     const formattedDate = `${year}${month}${day}`;
     return formattedDate;
   }
-  // const handleOutsideClick = (e: React.MouseEvent<HTMLDivElement>) => {
-  //   // 모달 바깥을 클릭하면 모달을 닫음
-  //   if (e.target === e.currentTarget) {
-  //     setModalOpen(false);
-  //     showScroll(); // 필요에 따라 모달이 닫힐 때 실행할 추가 작업을 수행할 수 있습니다.
-  //   }
-  // };
-
+  const handleOutsideClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.target === e.currentTarget) {
+      setModalOpen(false);
+      showScroll();
+    }
+  };
   const toggleLike = () => {
     if (!isLogin) {
       alert('로그인이 필요합니다.');
@@ -93,7 +91,7 @@ export const CustomModal = ({
   if (status === 'error') return <div>error...</div>;
   return (
     <S.Presentation>
-      <S.WrapperModal ref={ref}>
+      <S.WrapperModal onClick={handleOutsideClick}>
         <S.Modal>
           <S.ModalClose
             onClick={() => {
